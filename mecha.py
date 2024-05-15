fe852d174f0d704ff308b15cdf8f7775e268b6d5
istvanloffler <loffleristvan120@gmail.com>
2024-05-15 18:26:07 +0200

    3 b, Kész

--- mecha.py
+++ mecha.py
@@ -27,21 +27,21 @@ szamlalo = k_m*N
 gsz = 1/s #gerjesztés
 
 W_1 = (k_m*N)/(P11*s**2+P12*s+P13)*gsz
-print(W_1)
+print("WUom(s) = ",W_1)
 
 W_1_invL = sym.integrals.inverse_laplace_transform(W_1, s, t)
-print(W_1_invL)
+print("omegat és Ube közötti átmeneti fgv = ",W_1_invL)
 #sym.plot(W_1_invL,(t,0,0.2), ylabel='szögsebesség[rad/s]', xlabel='idő[s]')
 
 #1 f, feladat
 s12 = sym.solve(nevezo,s)
-print(s12)
+print("Gyökök (S1 ; s2) = ",s12)
 
 W_1s = szamlalo/((s-s12[0])*(s-s12[1]))
 T1 = -1/s12[1]
 T2 = -1/s12[0]
-print(W_1s)
-print([T1,T2])
+print("WUom = ",W_1s)
+print("Időállandók (T1 ; T2) = ",[T1,T2])
 
 
 #2 b, feladat
@@ -55,16 +55,14 @@ lim_W_Ui = sym.limit(W_Ui,s,0)
 lim_W_MOm = sym.limit(W_MOm,s,0)
 lim_W_Mi = sym.limit(W_Mi,s,0)
 
-print(lim_W_UOm)
-print(lim_W_Ui)
-print(lim_W_MOm)
-print(lim_W_Mi)
+print("limes WUomega = ",lim_W_UOm)
+print("limes WUI = ",lim_W_Ui)
+print("limes WMomega = ",lim_W_MOm)
+print("limes WMI = ",lim_W_Mi)
 
 #2 c, fleadat
-W_1_invLalg = sym.integrals.inverse_laplace_transform(W_1/gsz,s,t)
 W_1s_invLalg = sym.integrals.inverse_laplace_transform(W_1s,s,t)
-print(W_1_invLalg)
-print(W_1s_invLalg)
+print("Wuomega = ",W_1s_invLalg)
 
 #2 d, feladat
 W_Omt = W_1*U_be
@@ -74,7 +72,7 @@ W_Omt = sym.integrals.inverse_laplace_transform(W_Omt,s,t)
 
 #2 e, feladat
 Mmax = abs((i_n-U_be*lim_W_Ui)/lim_W_Mi)
-print(Mmax)
+print("Mmax = ",Mmax)
 
 
 #3 a, feladat
@@ -88,11 +86,11 @@ C1 = 0
 C2 = 1
 D = 0
 
-print(A11)
-print(A12)
-print(A21)
-print(A22)
-print(B1)
+print("A11a = ",A11)
+print("A12a = ",A12)
+print("A21a = ",A21)
+print("A22a = ",A22)
+print("B1a = ",B1)
 
 
 #3 b, feladat
@@ -100,24 +98,17 @@ Ab = sym.Matrix(2,2,[-R/L,-k_m/L,k_m/J_r,0])
 I = sym.Matrix(2,2,[1,0,0,1])
 M1 = I-Ab*dt
 Abd = M1.inv()
-A11b = 1+(R/L)*dt
-A12b = k_m/L*dt
-A21b = -k_m/J_r*dt
-A22b = 1*dt
-M = sym.Matrix(2,2,[A11b,A12b,A21b,A22b])
-M_inverse = M.inv()
 Bb = sym.Matrix(2,1,[(1/L),0])
-Bbd = M_inverse*Bb
+Bbd = Abd*Bb*dt
 Cb = sym.Matrix([0,1])
-Cbd = M_inverse*Cb
+Cbd = Abd*Cb
 Db = sym.Matrix([0,0])
 Dbd = sym.Matrix([Cb[0]*Bbd[0],Cb[1]*Bbd[1]])
 
-print(M_inverse)
-print(Abd)
-print(Bbd)
-print(Cbd)
-print(Dbd)
+print("Ad Matrix = ",Abd)
+print("Bd Matrix = ",Bbd)
+print("Cd Matrix = ",Cbd)
+print("Dd Matrix = ",Dbd)
 
 
 
@@ -135,7 +126,7 @@ lim_W_Ui2 = sym.limit(W_Ui2,s,0)
 lim_W_Mi2 = sym.limit(W_Mi2,s,0)
 
 Mmax2 = abs((i_n-U_be*lim_W_Ui2)/lim_W_Mi2)
-print(Mmax2)
+print("Mt B_r*10 estén = ",Mmax2)
 
 
 P11_3 = L*(J_r*N**2+J_t)
@@ -150,4 +141,4 @@ lim_W_Ui3 = sym.limit(W_Ui3,s,0)
 lim_W_Mi3 = sym.limit(W_Mi3,s,0)
 
 Mmax3 = abs((i_n-U_be*lim_W_Ui3)/lim_W_Mi3)
-print(Mmax3)
+print("Mt B_r*/10 estén = ",Mmax3)
